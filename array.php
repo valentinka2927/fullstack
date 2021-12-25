a96778ba43be4a138121f5bd81dc7e8e861a1c0a
valentinka2927 <valentinka2927@gmail.com>
2021-12-25 19:07:33 +0300

    готово

--- array.php
+++ array.php
@@ -29,15 +29,16 @@ $new_numbers[67]=67*67;
  $person = [
    'name' => 'Valentina',
     'age' => 32,
+    'city' => 'London',
     'language' => 'PHP'
  ];
- echo '<pre>';
- print_r($person);
+echo '<pre>';
+print_r($person);
  
  $person['car'] = true;
  $person['name'] = ' ';
 
- echo '<pre>';
+echo '<pre>';
  print_r($person);
   
  unset ($person ['name']);
@@ -45,6 +46,120 @@ $new_numbers[67]=67*67;
  print_r($person);
   
 
+echo '<hr>';
+$colors = ['red', 'green', 'blue'];
 
+ $output = ' ';
+ foreach ($colors as $color) {
+     $output = $output . ' ' . $color;
+ }
+
+
+ $string_colors = implode(', ', $colors );
+echo "$string_colors <br>";
+$new_str = 'Предложенеи из 4 слов';
+$array = explode(' ', $new_str);
+print_r($array);
+$numbers = [2, 4, 6, 1, 3];
+print_r($numbers);
+
+sort($numbers);
+print_r($numbers);
+
+print_r($person);
+
+sort($person);
+
+print_r($person);
+$person = [
+    'name' => 'Valentina',
+     'age' => 32,
+     'city' => 'London',
+     'language' => 'PHP'
+  ];
+  asort ($person);
+  print_r($person);
+
+ksort ($person);
+print_r($person);
  
- 
+rsort($numbers);
+ print_r($numbers); 
+
+ arsort($person);
+ print_r($person);
+
+ krsort($person);
+ print_r($person);
+ echo '<hr>';
+ $persons = [
+     [
+        'name' => 'Valentina',
+        'age' => 32,
+        'city' => 'London',
+        'languages' => ['PHP', 'JS']
+     ],
+     [
+        'name' => 'Jhon',
+        'age' => 20,
+        'city' => 'Madrid', 
+     ],
+     [
+        'name' => 'Petr',
+        'age' => 35,
+        'city' => 'Kazan',
+     ]
+    ];
+    function sortByAge ($a, $b) {
+ if ($a ['age'] > $b ['age']) {
+     return 1;
+ } else if ($a ['age'] > $b ['age']) {
+     return -1;
+ } else {
+     return 0;
+ }
+    }
+    usort($persons, 'sortByAge');
+    print_r($persons);
+
+    function sortByCity ($a, $b) {
+        return $a['city'] <=> $b['city'];
+    }
+    print_r($persons);
+
+    usort($persons, 'sortByCity');
+    print_r($persons);
+
+    echo '<hr>';
+
+    $numberOne = 15;
+    $numberTwo = 15;
+    
+    // if  ($numberOne > $numberTwo) {
+    //     echo 1;
+    // } else if ($numberOne < $numberTwo) {
+    //      echo -1;
+    //  } else {
+    //     echo 0;
+    // }
+echo $numberOne <=> $numberTwo;
+
+$names = array_column($persons, 'name');
+print_r($names);
+
+array_multisort($names, SORT_DESC, $persons);
+print_r($persons);
+
+$keys = array_keys($person);
+print_r($keys);
+
+$values = array_values($person);
+print_r($values);
+
+print_r($persons[0]['languages'][0][0]);
+
+echo '<br>';
+print_r($persons[0]['name']);
+echo '<br>';
+
+print_r($persons[1]['age']);
