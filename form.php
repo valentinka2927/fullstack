2a032e18fed982ce1a83a75c6ca46d793805cab1
valentinka2927 <valentinka2927@gmail.com>
2022-01-10 19:36:48 +0300

    форма создания пользователя

--- form.php
+++ form.php
@@ -1,6 +1,22 @@
 <?php
 
+$user = $password = 'root';
+$pdo = new Pdo('mysql:dbname=fullstac2; fulstack=127.0.0.1', $user);
+
 $name = $_POST['name'];
-$age = $_POST['age'];
- echo "Hello, $name, you are $age years old!!";
+$login = $_POST['login'];
+$password = $_POST['password'];
+$city_id = $_POST['city_id'];
+
+ $query = "INSERT INTO users (name, login, password, city_id) VALUES(:name, :login, :password, :city_id)";
+ $res = $pdo->prepare($query);
+ $res->execute([
+     ':name' =>$name,
+     ':login' =>$login,
+     ':password' =>$password,
+     ':city_id' =>$city_id
+ ]);
+
+header ('location: index.php');
+ 
  
--- index.php
+++ index.php
@@ -1,3 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+       <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class='container mt-5'>
+
 <?php
 
+$user = $password = 'root';
+$pdo = new Pdo('mysql:dbname=fullstac2; fulstack=127.0.0.1', $user);
 
+$query = "SELECT * FROM users";
+$res = $pdo->query($query);
+$users = $res->fetchAll();
+
+$query = "SELECT * FROM cities";
+$res = $pdo->query($query);
+$cities = $res->fetchAll();
+
+
+echo "
+<table class='table table-bordered'>
+       <thead>
+            <tr>
+                <th>id</th>
+                <th>Логин</th>
+                <th>Имя</th>
+                <th>id города</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+
+";
+foreach ($users as $user) {
+   // if ($user ['city_id']){
+   //     $city = $user['city_id'];
+   // } else {
+   //    $city = '-';
+   // }
+
+$city = $user['city_id'] ? $user['city_id'] : '-';
+
+    echo  "
+      <tr>
+         <td>{$user['id']}</td>
+         <td>{$user['login']}</td>
+         <td>{$user['name']}</td>
+         <td>{$user['city_id']}</td>
+         <td>{$city}</td>
+         <td>
+             <form method='post' action='del_user.php'>
+               <button class='btn btn-danger'>x</button>
+             </form>
+         </td>
+         <td>
+    </tr>
+ ";
+}
+echo "</tbody>
+</table>";
+
+?>
+           
+            <form method="POST" action="form.php">
+                <input required class="form-control mb-2" placeholder="Имя" name='name'>
+                <input class="form-control mb-2" placeholder="Логин" name='login'>
+                <input class="form-control mb-2"type="password" placeholder="Пароль" name='password'>
+               <select class="form-control mb-2" name="city_id">
+                   <option  selected disabled>-- Выберите город --</option>
+                   <?php
+                        foreach ($cities as $city) {
+                            echo "<option value='{$city['id']}'>{$city['name']}</option>";
+                        }
+                    ?>
+                </select>
+                <button type="submit" class="btn btn-success w-100">Отправить</button>
+  
+
+            </form>
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
